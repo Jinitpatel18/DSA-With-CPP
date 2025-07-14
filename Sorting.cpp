e5ab773c0cde5641aaf2326726fb77658d971679
Jinitpatel18 <jinitpatel188@gmail.com>
2025-07-14 09:27:50 +0530

    Insertion sort, Bubble sort, Selection sort

---
+++ Sorting.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+        while (prev >= 0 && arr[prev] > curr)
+        {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {4, 1, 2, 5, 3};
+    int n = 5;
+    // bubbleSort(arr,n);
+    // selectionSort(arr,n);
+    insertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
